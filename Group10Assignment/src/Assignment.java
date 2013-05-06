156d581f8249ce98113e76113efed96cb62bb89e
dilarahae@gmail.com <dilarahae@gmail.com@b9fef26f-2b5a-7500-4f01-d936de0847ac>
2013-05-06 11:56:32 +0000

    secondary commit to check

---
+++ Group10Assignment/src/Assignment.java
@@ -0,0 +1,16 @@
+
+public class Assignment {
+
+	
+	public static void main(String[] args) {
+		int d=Dilara();
+	}
+	public static int Dilara(){
+		System.out.println("There was nothing more...");
+		System.out.println("Nihongo de kakou to omotta kedo, dekinakatta. minnasan yoroshiku onegaishimasu!!");
+		int denakatta=5;
+		return denakatta;
+		
+	}
+
+}
