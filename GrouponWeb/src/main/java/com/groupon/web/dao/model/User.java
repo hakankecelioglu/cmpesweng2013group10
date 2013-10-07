e0ff0c9e2345b123f83ef138b2d0814cedc4eb9f
serdarkuzucu13@gmail.com <serdarkuzucu13@gmail.com@b9fef26f-2b5a-7500-4f01-d936de0847ac>
2013-10-07 20:55:17 +0000

    code format and bug fix

--- GrouponWeb/src/main/java/com/groupon/web/dao/model/User.java
+++ GrouponWeb/src/main/java/com/groupon/web/dao/model/User.java
@@ -9,111 +9,111 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "user")
 public class User {
-    @Id
-    @GeneratedValue
-    @Column(name = "id")
-    private long id;
+	@Id
+	@GeneratedValue
+	@Column(name = "id")
+	private long id;
 
-    @Column(name = "username", unique = true)
-    private String username;
+	@Column(name = "username", unique = true)
+	private String username;
 
-    @Column(name = "email", unique = true)
-    private String email;
+	@Column(name = "email", unique = true)
+	private String email;
 
-    @Column(name = "name", nullable = true)
-    private String name;
+	@Column(name = "name", nullable = true)
+	private String name;
 
-    @Column(name = "surname", nullable = true)
-    private String surname;
+	@Column(name = "surname", nullable = true)
+	private String surname;
 
-    @Column(name = "password")
-    private String password;
+	@Column(name = "password")
+	private String password;
 
-    public long getId() {
-	return id;
-    }
+	public long getId() {
+		return id;
+	}
 
-    public void setId(long id) {
-	this.id = id;
-    }
+	public void setId(long id) {
+		this.id = id;
+	}
 
-    public String getUsername() {
-	return username;
-    }
+	public String getUsername() {
+		return username;
+	}
 
-    public void setUsername(String username) {
-	this.username = username;
-    }
+	public void setUsername(String username) {
+		this.username = username;
+	}
 
-    public String getEmail() {
-	return email;
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    public void setEmail(String email) {
-	this.email = email;
-    }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-    public String getName() {
-	return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-	this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public String getSurname() {
-	return surname;
-    }
+	public String getSurname() {
+		return surname;
+	}
 
-    public void setSurname(String surname) {
-	this.surname = surname;
-    }
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
 
-    public String getPassword() {
-	return password;
-    }
+	public String getPassword() {
+		return password;
+	}
 
-    public void setPassword(String password) {
-	this.password = password;
-    }
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
-    @Override
-    public int hashCode() {
-	final int prime = 31;
-	int result = 1;
-	result = prime * result + (int) (id ^ (id >>> 32));
-	return result;
-    }
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-	if (this == obj)
-	    return true;
-	if (obj == null)
-	    return false;
-	if (getClass() != obj.getClass())
-	    return false;
-	User other = (User) obj;
-	if (id != other.id)
-	    return false;
-	return true;
-    }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
 
-    @Override
-    public String toString() {
-	StringBuilder builder = new StringBuilder();
-	builder.append("User [id=");
-	builder.append(id);
-	builder.append(", username=");
-	builder.append(username);
-	builder.append(", email=");
-	builder.append(email);
-	builder.append(", name=");
-	builder.append(name);
-	builder.append(", surname=");
-	builder.append(surname);
-	builder.append("]");
-	return builder.toString();
-    }
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("User [id=");
+		builder.append(id);
+		builder.append(", username=");
+		builder.append(username);
+		builder.append(", email=");
+		builder.append(email);
+		builder.append(", name=");
+		builder.append(name);
+		builder.append(", surname=");
+		builder.append(surname);
+		builder.append("]");
+		return builder.toString();
+	}
 
 }
--- GrouponWeb/src/main/java/com/groupon/web/util/GrouponWebUtils.java
+++ GrouponWeb/src/main/java/com/groupon/web/util/GrouponWebUtils.java
@@ -6,35 +6,35 @@ import java.security.MessageDigest;
 import com.groupon.web.dao.model.User;
 
 public class GrouponWebUtils {
-    public static String generateCookieForUser(User user) {
-	StringBuilder stringBuilder = new StringBuilder();
-	stringBuilder.append(user.getId());
-	stringBuilder.append("#");
-	stringBuilder.append(generateUserHash(user));
-	return stringBuilder.toString();
-    }
-    
-    public static String generateUserHash(User user) {
-	StringBuilder toBeHashed = new StringBuilder();
-	toBeHashed.append("abcdef123456789jfuejdhs!'?^='?+)=%&)%&");
-	toBeHashed.append(user.getEmail());
-	toBeHashed.append(user.getId());
-	toBeHashed.append(user.getUsername());
-	return md5(toBeHashed.toString());
-    }
-
-    public static String md5(String md5) {
-	try {
-	    MessageDigest md = MessageDigest.getInstance("MD5");
-	    byte[] array = md.digest(md5.getBytes("UTF-8"));
-	    StringBuffer sb = new StringBuffer();
-	    for (int i = 0; i < array.length; ++i) {
-		sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100).substring(1, 3));
-	    }
-	    return sb.toString();
-	} catch (java.security.NoSuchAlgorithmException e) {
-	} catch (UnsupportedEncodingException e) {
+	public static String generateCookieForUser(User user) {
+		StringBuilder stringBuilder = new StringBuilder();
+		stringBuilder.append(user.getId());
+		stringBuilder.append("#");
+		stringBuilder.append(generateUserHash(user));
+		return stringBuilder.toString();
 	}
-	return null;
-    }
+
+	public static String generateUserHash(User user) {
+		StringBuilder toBeHashed = new StringBuilder();
+		toBeHashed.append("abcdef123456789jfuejdhs!'?^='?+)=%&)%&");
+		toBeHashed.append(user.getEmail());
+		toBeHashed.append(user.getId());
+		toBeHashed.append(user.getUsername());
+		return md5(toBeHashed.toString());
+	}
+
+	public static String md5(String md5) {
+		try {
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			byte[] array = md.digest(md5.getBytes("UTF-8"));
+			StringBuffer sb = new StringBuffer();
+			for (int i = 0; i < array.length; ++i) {
+				sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100).substring(1, 3));
+			}
+			return sb.toString();
+		} catch (java.security.NoSuchAlgorithmException e) {
+		} catch (UnsupportedEncodingException e) {
+		}
+		return null;
+	}
 }
