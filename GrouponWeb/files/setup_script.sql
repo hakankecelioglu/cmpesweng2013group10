f0282658220aa299a7ba8f7bad78e770ecf9a7df
serdarkuzucu13@gmail.com <serdarkuzucu13@gmail.com@b9fef26f-2b5a-7500-4f01-d936de0847ac>
2013-10-30 21:11:33 +0000

    setup script updated

--- GrouponWeb/files/setup_script.sql
+++ GrouponWeb/files/setup_script.sql
@@ -1,10 +1,10 @@
 create database groupon character set 'utf8';
 
-insert into role (id, name) values (1, 'ADMIN');
-insert into role (id, name) values (2, 'MODERATOR');
-insert into role (id, name) values (3, 'USER');
+insert into role (id, name, create_date, update_date) values (1, 'ADMIN', sysdate(), sysdate());
+insert into role (id, name, create_date, update_date) values (2, 'MODERATOR', sysdate(), sysdate());
+insert into role (id, name, create_date, update_date) values (3, 'USER', sysdate(), sysdate());
 
 -- password: 123456
-insert into user (id, email, username, name, surname, password, rating) values (1, 'admin@admin.com', 'admin', 'admin', 'admin', '85bce29c3868a29d04b7e62f1c0451b0', 1000000);
+insert into user (id, email, username, name, surname, password, rating, create_date, update_date) values (1, 'admin@admin.com', 'admin', 'admin', 'admin', '85bce29c3868a29d04b7e62f1c0451b0', 1000000, sysdate(), sysdate());
 
-insert into user_role (id, role_id, user_id) values (1, 1, 1);
+insert into user_role (id, role_id, user_id, create_date, update_date) values (1, 1, 1, sysdate(), sysdate());
