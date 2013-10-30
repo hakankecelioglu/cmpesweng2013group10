13b974766f43d09e5c3dce7bb32ea9cef05f76fc
serkanozer.591@gmail.com <serkanozer.591@gmail.com@b9fef26f-2b5a-7500-4f01-d936de0847ac>
2013-10-30 23:52:44 +0000

    all tasks and communities are pulled instead of dummy content

--- GrouponWeb/src/main/java/com/groupon/web/controller/HomeController.java
+++ GrouponWeb/src/main/java/com/groupon/web/controller/HomeController.java
@@ -3,13 +3,22 @@ package com.groupon.web.controller;
 import javax.servlet.http.HttpServletRequest;
 
 import org.json.JSONException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.groupon.web.dao.model.User;
+import com.groupon.web.service.CommunityService;
+import com.groupon.web.service.TaskService;
+
 @Controller
 public class HomeController extends AbstractBaseController {
+	@Autowired
+	private TaskService taskService;
+	@Autowired
+	private CommunityService communityService;
 
 	/**
 	 * Opens home page
@@ -21,6 +30,14 @@ public class HomeController extends AbstractBaseController {
 	public Object home(HttpServletRequest request, Model model) throws JSONException {
 		setGlobalAttributesToModel(model, request);
 		model.addAttribute("page", "home");
+		User user = getUser(request);
+		model.addAttribute("alltasks", taskService.getAllTasks());
+		if (user == null) {
+			model.addAttribute("allcommunities", communityService.getAllCommunities());
+			return "homeguest.view";
+		}
+
+		// generateDummyContent(model);
 		return "home.view";
 	}
 }
