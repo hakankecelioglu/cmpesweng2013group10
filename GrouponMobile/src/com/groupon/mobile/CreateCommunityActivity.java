0c6e6a4263dcbb916cf755e894ba61a08909afe2
serdarkuzucu13@gmail.com <serdarkuzucu13@gmail.com@b9fef26f-2b5a-7500-4f01-d936de0847ac>
2013-10-31 01:23:21 +0000

    create task activity created

--- GrouponMobile/src/com/groupon/mobile/CreateCommunityActivity.java
+++ GrouponMobile/src/com/groupon/mobile/CreateCommunityActivity.java
@@ -1,6 +1,7 @@
 package com.groupon.mobile;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -9,7 +10,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class CreateCommunityActivity extends Activity {
-	
+
 	private Button createButton;
 	private EditText communityNameField;
 	private EditText communityDescriptionField;
@@ -18,36 +19,39 @@ public class CreateCommunityActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_create_community);
-		
+
 		setupUI();
 	}
 
 	private void setupUI() {
 		createButton = (Button) findViewById(R.id.button_create_community);
 		createButton.setOnClickListener(createButtonClickListener);
-		
+
 		communityNameField = (EditText) findViewById(R.id.community_name);
 		communityDescriptionField = (EditText) findViewById(R.id.community_description);
 	}
-	
+
 	private OnClickListener createButtonClickListener = new OnClickListener() {
 		@Override
 		public void onClick(View v) {
 			String name = communityNameField.getText().toString();
 			String description = communityDescriptionField.getText().toString();
-			
+
 			if (name.trim().equals("")) {
 				Toast.makeText(CreateCommunityActivity.this, "Community name cannot be empty!", Toast.LENGTH_SHORT).show();
 				return;
 			}
-			
+
 			if (description.trim().equals("")) {
 				Toast.makeText(CreateCommunityActivity.this, "Community description cannot be empty!", Toast.LENGTH_SHORT).show();
 				return;
 			}
-			
-			int id = DummyController.createCommunity(name, description);
-			//Intent intent = new Intent();
+
+			int communityId = DummyController.createCommunity(name, description);
+			Intent intent = new Intent(CreateCommunityActivity.this, CommunityActivity.class);
+			intent.putExtra("communityId", communityId);
+			startActivity(intent);
+			finish();
 		}
 	};
 
