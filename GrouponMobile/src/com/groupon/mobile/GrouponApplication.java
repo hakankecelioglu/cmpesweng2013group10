dec4b8874e5cd9d2042ca53c3fd8be0f1e209341
serdarkuzucu13@gmail.com <serdarkuzucu13@gmail.com@b9fef26f-2b5a-7500-4f01-d936de0847ac>
2013-10-31 01:05:39 +0000

    mockup screens implemented as xml

---
+++ GrouponMobile/src/com/groupon/mobile/GrouponApplication.java
@@ -0,0 +1,46 @@
+package com.groupon.mobile;
+
+import com.groupon.mobile.model.User;
+
+import android.app.Activity;
+import android.app.Application;
+import android.content.SharedPreferences;
+
+public class GrouponApplication extends Application {
+	private User user;
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		user = readLoggedUser();
+		DummyController.init();
+	}
+
+	public User getLoggedUser() {
+		return user;
+	}
+
+	public void setLoggedUser(User user) {
+		this.user = user;
+		writeLoggedUser(user);
+	}
+
+	private User readLoggedUser() {
+		SharedPreferences preferences = getSharedPreferences("user", Activity.MODE_PRIVATE);
+		String username = preferences.getString("username", null);
+		if (username == null) {
+			return null;
+		}
+
+		User user = new User();
+		user.setUsername(username);
+		return user;
+	}
+
+	private void writeLoggedUser(User user) {
+		SharedPreferences preferences = getSharedPreferences("user", Activity.MODE_PRIVATE);
+		SharedPreferences.Editor editor = preferences.edit();
+		editor.putString("username", user.getUsername());
+		editor.commit();
+	}
+}
---
+++ GrouponMobile/src/com/groupon/mobile/HomeActivity.java
@@ -0,0 +1,43 @@
+package com.groupon.mobile;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.groupon.mobile.model.User;
+
+public class HomeActivity extends Activity {
+	private User user;
+	private Button createCommunityTaskButton;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_home);
+
+		GrouponApplication app = (GrouponApplication) getApplication();
+		user = app.getLoggedUser();
+		
+		TextView view = (TextView) findViewById(R.id.home_hello_username);
+		view.setText("Hello, " + user.getUsername() + "!");
+		
+		setupUI();
+	}
+	
+	private void setupUI() {
+		createCommunityTaskButton = (Button) findViewById(R.id.button_home_create_new_community);
+		createCommunityTaskButton.setOnClickListener(createNewCommunityListener);
+	}
+	
+	private OnClickListener createNewCommunityListener = new OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			Intent intent = new Intent(HomeActivity.this, CreateCommunityActivity.class);
+			startActivity(intent);
+		}
+	};
+}
