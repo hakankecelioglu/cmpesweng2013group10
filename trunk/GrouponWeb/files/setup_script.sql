9c1da07036a0bfb482251654bb7684aab7b9d4fd
serdarkuzucu13@gmail.com <serdarkuzucu13@gmail.com@b9fef26f-2b5a-7500-4f01-d936de0847ac>
2013-11-06 00:56:48 +0000

    setup script updated according to new user model

--- trunk/GrouponWeb/files/setup_script.sql
+++ trunk/GrouponWeb/files/setup_script.sql
@@ -5,6 +5,6 @@ insert into role (id, name, create_date, update_date) values (2, 'MODERATOR', sy
 insert into role (id, name, create_date, update_date) values (3, 'USER', sysdate(), sysdate());
 
 -- password: 123456
-insert into user (id, email, username, name, surname, password, rating, create_date, update_date) values (1, 'admin@admin.com', 'admin', 'admin', 'admin', '85bce29c3868a29d04b7e62f1c0451b0', 1000000, sysdate(), sysdate());
+insert into user (id, email, username, name, surname, password, rating, status, create_date, update_date) values (1, 'admin@admin.com', 'admin', 'admin', 'admin', '85bce29c3868a29d04b7e62f1c0451b0', 1000000, 'ACTIVE', sysdate(), sysdate());
 
 insert into user_role (id, role_id, user_id, create_date, update_date) values (1, 1, 1, sysdate(), sysdate());
