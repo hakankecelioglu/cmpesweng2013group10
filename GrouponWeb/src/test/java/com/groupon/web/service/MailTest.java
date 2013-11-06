d24c6b111cb5bfc632a0417dd3ac0998358b648a
serdarkuzucu13@gmail.com <serdarkuzucu13@gmail.com@b9fef26f-2b5a-7500-4f01-d936de0847ac>
2013-11-06 00:55:58 +0000

    signup mailing added.

---
+++ GrouponWeb/src/test/java/com/groupon/web/service/MailTest.java
@@ -0,0 +1,49 @@
+package com.groupon.web.service;
+
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.AddressException;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+
+import com.groupon.web.AbstractGrouponTest;
+import com.groupon.web.controller.GrouponException;
+import com.groupon.web.dao.model.User;
+import com.groupon.web.util.GrouponWebUtils;
+
+public class MailTest extends AbstractGrouponTest {
+	@Autowired
+	private MailService mailService;
+
+	@Autowired
+	private UserService userService;
+
+	@Value("${SITE_URL}")
+	private String SITE_URL;
+
+	@Test
+	public void sendWelcomeMail() throws GrouponException {
+		User user = userService.getUserByUsernameAndPassword("admin", GrouponWebUtils.hashPasswordForDB("123456"));
+		assertNotNull(user);
+
+		String userCode = GrouponWebUtils.generateUserHash(user);
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("signup_approval_link", SITE_URL + "emailApproval?code=" + userCode);
+		params.put("signup_username", "Crazy MC");
+
+		try {
+			mailService.sendEmail("mails/signupMail.vm", "serdarkuzucu13@gmail.com", "Test Welcome", params, "serdar.kuzucu@inomera.com");
+		} catch (AddressException e) {
+			e.printStackTrace();
+		} catch (MessagingException e) {
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+	}
+}
