63e78349121d0390672d843688bc6677dcff97e6
serdarkuzucu13@gmail.com <serdarkuzucu13@gmail.com@b9fef26f-2b5a-7500-4f01-d936de0847ac>
2013-11-26 16:49:40 +0000

    home page tasks were organized.

--- GrouponWeb/src/main/java/com/groupon/web/controller/HomeController.java
+++ GrouponWeb/src/main/java/com/groupon/web/controller/HomeController.java
@@ -1,21 +1,14 @@
 package com.groupon.web.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.commons.lang3.StringUtils;
 import org.json.JSONException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import com.groupon.web.dao.model.Community;
 import com.groupon.web.dao.model.User;
 import com.groupon.web.service.CommunityService;
 import com.groupon.web.service.TaskService;
@@ -24,6 +17,7 @@ import com.groupon.web.service.TaskService;
 public class HomeController extends AbstractBaseController {
 	@Autowired
 	private TaskService taskService;
+
 	@Autowired
 	private CommunityService communityService;
 
@@ -31,28 +25,23 @@ public class HomeController extends AbstractBaseController {
 	 * Opens home page
 	 * 
 	 * @return the view which will be shown on homepage
-	 * @throws JSONException
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public Object home(HttpServletRequest request, Model model) throws JSONException {
+	public Object home(HttpServletRequest request, Model model) {
 		setGlobalAttributesToModel(model, request);
 		model.addAttribute("page", "home");
+
 		User user = getUser(request);
-		model.addAttribute("alltasks", taskService.getAllTasks());
 		if (user == null) {
 			model.addAttribute("allcommunities", communityService.getAllCommunities());
 			return "homeguest.view";
 		}
 
-		// generateDummyContent(model);
+		model.addAttribute("followedTasks", taskService.getFollowedTasks(user));
+		model.addAttribute("communityTasks", taskService.getCommunityTasks(user));
+		model.addAttribute("recommendedTasks", taskService.getRecommendedTasks(user));
+
 		return "home.view";
 	}
 
-	@RequestMapping(value = "/dummy", method = RequestMethod.GET)
-	public ResponseEntity<Map<String, Object>> createCommunityAndroid(HttpServletRequest request) {
-		Map<String, Object> response = new HashMap<String, Object>();
-		response.put("connection", "ok");
-		return prepareSuccessResponse(response);
-
-	}
 }
