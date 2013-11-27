03478ccfcbe72f32a20421b10a72d98bab948c78
serdarkuzucu13@gmail.com <serdarkuzucu13@gmail.com@b9fef26f-2b5a-7500-4f01-d936de0847ac>
2013-11-27 23:01:21 +0000

    source code format and fix

--- trunk/GrouponMobile/src/com/groupon/mobile/CommunityAdapter.java
+++ trunk/GrouponMobile/src/com/groupon/mobile/CommunityAdapter.java
@@ -1,10 +1,6 @@
 package com.groupon.mobile;
 
 import java.util.List;
- 
-
-
-import com.groupon.mobile.model.Community;
 
 import android.content.Context;
 import android.view.LayoutInflater;
@@ -13,44 +9,41 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
 import android.widget.TextView;
- 
- 
+
+import com.groupon.mobile.model.Community;
+
 public class CommunityAdapter extends ArrayAdapter<Community> {
- 
-    int resource;
-    String response;
-    Context context;
-    public CommunityAdapter(Context context, int resource, List<Community> items) {
-        super(context, resource, items);
-        this.resource=resource;
-    }
-    public View getView(int position, View convertView, ViewGroup parent)
-    {
-        LinearLayout alertView;
 
-        Community community = getItem(position);
-         
+	int resource;
+	String response;
+	Context context;
 
-        if(convertView==null)
-        {
-            alertView = new LinearLayout(getContext());
-            String inflater = Context.LAYOUT_INFLATER_SERVICE;
-            LayoutInflater vi;
-            vi = (LayoutInflater)getContext().getSystemService(inflater);
-            vi.inflate(resource, alertView, true);
-        }
-        else
-        {
-            alertView = (LinearLayout) convertView;
-        }
+	public CommunityAdapter(Context context, int resource, List<Community> items) {
+		super(context, resource, items);
+		this.resource = resource;
+	}
 
-        TextView communityName =(TextView)alertView.findViewById(R.id.community_name);
-        TextView communityDescription =(TextView)alertView.findViewById(R.id.community_description);
-         
+	public View getView(int position, View convertView, ViewGroup parent) {
+		LinearLayout alertView;
 
-        communityName.setText(community.getName());
-        communityDescription.setText(community.getDescription());
-         
-        return alertView;
-    }
+		Community community = getItem(position);
+
+		if (convertView == null) {
+			alertView = new LinearLayout(getContext());
+			String inflater = Context.LAYOUT_INFLATER_SERVICE;
+			LayoutInflater vi;
+			vi = (LayoutInflater) getContext().getSystemService(inflater);
+			vi.inflate(resource, alertView, true);
+		} else {
+			alertView = (LinearLayout) convertView;
+		}
+
+		TextView communityName = (TextView) alertView.findViewById(R.id.community_name);
+		TextView communityDescription = (TextView) alertView.findViewById(R.id.community_description);
+
+		communityName.setText(community.getName());
+		communityDescription.setText(community.getDescription());
+
+		return alertView;
+	}
 }
