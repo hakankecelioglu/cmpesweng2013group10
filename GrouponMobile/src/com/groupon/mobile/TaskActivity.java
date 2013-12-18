2f6577ced9d8a51c7289d5661d77689e8d416be2
serkanozer.591@gmail.com <serkanozer.591@gmail.com@b9fef26f-2b5a-7500-4f01-d936de0847ac>
2013-12-18 19:46:55 +0000

    Task attributes are added

--- GrouponMobile/src/com/groupon/mobile/TaskActivity.java
+++ GrouponMobile/src/com/groupon/mobile/TaskActivity.java
@@ -1,6 +1,8 @@
 package com.groupon.mobile;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
 import com.groupon.mobile.conn.GrouponCallback;
@@ -13,6 +15,7 @@ import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class TaskActivity extends BaseActivity {
@@ -20,16 +23,17 @@ public class TaskActivity extends BaseActivity {
 	private TextView taskDescriptionField;
 	private TextView taskDeadlineField;
 	private TextView taskOwner;
-
+	private TextView taskDescriptionLabel;
 	private TextView taskfollowercount;
 	private Button followTaskButton;
 	private long taskId;
 	private Task task;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		taskId = getIntent().getLongExtra("taskId", -1);
-		
+
 		setContentView(R.layout.activity_task);
 		setTask();
 
@@ -38,33 +42,53 @@ public class TaskActivity extends BaseActivity {
 	private void setupUI() {
 
 		taskNameField = (TextView) findViewById(R.id.task_name);
-		taskNameField.setText(task.getName()+" in "+task.getCommunityName() );
+		taskNameField.setText(task.getName() + " in " + task.getCommunityName());
+
 		taskDescriptionField = (TextView) findViewById(R.id.task_description);
 		taskDescriptionField.setText(task.getDescription());
+		taskDescriptionLabel = (TextView) findViewById(R.id.task_description_info);
+		taskDescriptionLabel.setVisibility(View.VISIBLE);
 		taskDeadlineField = (TextView) findViewById(R.id.task_deadline);
-		taskDeadlineField.setText(task.getDeadlineCount().toString()+" days left");
-		taskOwner = (TextView)	findViewById(R.id.task_owner);
-		taskOwner.setText("by "+task.getOwnerUsername());
+		taskDeadlineField.setText(task.getDeadlineCount().toString() + " days left");
+		taskOwner = (TextView) findViewById(R.id.task_owner);
+		taskOwner.setText("by " + task.getOwnerUsername());
 
 		String needType = task.getNeedType();
 		TextView requirement = (TextView) findViewById(R.id.task_requirement);
-		if(needType.equals("GOODS")){
-			
-			requirement.setText(task.getRequirementQuantity()+" more "+task.getRequirementName()+" needed");
-		}
-		else if(needType.equals("SERVICE")){
-			requirement.setText(task.getRequirementName()+" needed");
+		if (needType.equals("GOODS")) {
+
+			requirement.setText(task.getRequirementQuantity() + " more " + task.getRequirementName() + " needed");
+		} else if (needType.equals("SERVICE")) {
+			requirement.setText(task.getRequirementName() + " needed");
 		}
 		taskfollowercount = (TextView) findViewById(R.id.task_follower_count);
-		taskfollowercount.setText(task.getFollowerCount()+" followers");
+		taskfollowercount.setText(task.getFollowerCount() + " followers");
 		followTaskButton = (Button) findViewById(R.id.task_follow_button);
-		if(!task.isFollower()){
-		followTaskButton.setOnClickListener(followTaskListener);
+		if (!task.isFollower()) {
+			followTaskButton.setOnClickListener(followTaskListener);
+		} else {
+			followTaskButton.setText("Unfollow");
+			followTaskButton.setOnClickListener(unFollowTaskListener);
 		}
-		else{
-		followTaskButton.setText("Unfollow");
-		followTaskButton.setOnClickListener(unFollowTaskListener);
+
+		Map<String, List<String>> m = task.getAttributeMap();
+		LinearLayout mainLayout = (LinearLayout) findViewById(R.id.mainLinear);
+		for (Map.Entry<String, List<String>> entry : m.entrySet()) {
+			String attributeName = entry.getKey();
+			List<String> AttributeValues = entry.getValue();
+			LinearLayout taskAttributeLayout = new LinearLayout(this.getApplicationContext());
+			taskAttributeLayout.setOrientation(LinearLayout.VERTICAL);
+			TextView attributeTextView = new TextView(this.getApplicationContext());
+			attributeTextView.setText(attributeName);
+			taskAttributeLayout.addView(attributeTextView);
+			for (String attributeValue : AttributeValues) {
+				TextView attributeNameView = new TextView(this.getApplicationContext());
+				attributeNameView.setText(attributeValue);
+				taskAttributeLayout.addView(attributeNameView);
+			}
+			mainLayout.addView(taskAttributeLayout);
 		}
+
 	}
 
 	@Override
@@ -73,9 +97,9 @@ public class TaskActivity extends BaseActivity {
 		getMenuInflater().inflate(R.menu.task, menu);
 		return true;
 	}
-	
+
 	private OnClickListener followTaskListener = new OnClickListener() {
-		
+
 		@Override
 		public void onClick(View v) {
 			TaskService taskService = new TaskService(getApp());
@@ -83,22 +107,21 @@ public class TaskActivity extends BaseActivity {
 
 				@Override
 				public void onSuccess(Task response) {
-					
+
 					followTaskButton.setText("unfollow");
 					followTaskButton.setOnClickListener(unFollowTaskListener);
-					taskfollowercount.setText(response.getFollowerCount()+" followers");
+					taskfollowercount.setText(response.getFollowerCount() + " followers");
 				}
 
 				@Override
 				public void onFail(String errorMessage) {
-					
-					
+
 				}
 			});
 		}
 	};
 	private OnClickListener unFollowTaskListener = new OnClickListener() {
-		
+
 		@Override
 		public void onClick(View v) {
 			TaskService taskService = new TaskService(getApp());
@@ -106,21 +129,21 @@ public class TaskActivity extends BaseActivity {
 
 				@Override
 				public void onSuccess(Task response) {
-					
+
 					followTaskButton.setText("follow");
 					followTaskButton.setOnClickListener(followTaskListener);
-					taskfollowercount.setText(response.getFollowerCount()+" followers");
+					taskfollowercount.setText(response.getFollowerCount() + " followers");
 				}
 
 				@Override
 				public void onFail(String errorMessage) {
-					
-					
+
 				}
 			});
 		}
 	};
-	private void setTask(){
+
+	private void setTask() {
 		TaskService taskService = new TaskService(getApp());
 		taskService.getTask(taskId, new GrouponCallback<Task>() {
 
@@ -128,15 +151,14 @@ public class TaskActivity extends BaseActivity {
 			public void onSuccess(Task task) {
 				TaskActivity.this.task = task;
 				setupUI();
-			
+
 			}
 
 			@Override
 			public void onFail(String errorMessage) {
-				
-				
+
 			}
 		});
-	
+
 	}
 }
