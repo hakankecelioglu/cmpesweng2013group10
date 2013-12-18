5876488eb167870b64e8dec3b87113d495e82914
serkanozer.591@gmail.com <serkanozer.591@gmail.com@b9fef26f-2b5a-7500-4f01-d936de0847ac>
2013-12-18 21:55:18 +0000

    followed tasks implementation

---
+++ GrouponMobile/src/com/groupon/mobile/TaskAdapter.java
@@ -0,0 +1,46 @@
+package com.groupon.mobile;
+
+import java.util.List;
+import com.groupon.mobile.model.Task;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class TaskAdapter extends ArrayAdapter<Task> {
+	int resource;
+	String response;
+	Context context;
+
+	public TaskAdapter(Context context, int resource, List<Task> items) {
+		super(context, resource, items);
+		this.resource = resource;
+	}
+
+	public View getView(int position, View convertView, ViewGroup parent) {
+		LinearLayout alertView;
+
+		Task task = getItem(position);
+
+		if (convertView == null) {
+			alertView = new LinearLayout(getContext());
+			String inflater = Context.LAYOUT_INFLATER_SERVICE;
+			LayoutInflater vi;
+			vi = (LayoutInflater) getContext().getSystemService(inflater);
+			vi.inflate(resource, alertView, true);
+		} else {
+			alertView = (LinearLayout) convertView;
+		}
+
+		TextView taskName = (TextView) alertView.findViewById(R.id.task_name);
+		TextView taskDescription = (TextView) alertView.findViewById(R.id.task_description);
+		taskName.setText(task.getName());
+		taskDescription.setText(task.getDescription());
+
+		return alertView;
+	}
+}
