e825b385db16e3e97404102eb8ae18b0e1c6c1b6
serkanozer.591@gmail.com <serkanozer.591@gmail.com@b9fef26f-2b5a-7500-4f01-d936de0847ac>
2013-12-18 21:55:18 +0000

    followed tasks implementation

--- trunk/GrouponMobile/src/com/groupon/mobile/service/TaskService.java
+++ trunk/GrouponMobile/src/com/groupon/mobile/service/TaskService.java
@@ -1,8 +1,9 @@
 package com.groupon.mobile.service;
 
-
-
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -19,6 +20,7 @@ import com.groupon.mobile.model.Community;
 import com.groupon.mobile.model.Task;
 import com.groupon.mobile.model.TaskAttribute;
 import com.groupon.mobile.utils.Constants;
+import com.groupon.mobile.utils.DateUtils;
 
 public class TaskService {
 	private GrouponApplication app;
@@ -26,36 +28,42 @@ public class TaskService {
 	public TaskService(GrouponApplication app) {
 		this.app = app;
 	}
-	public void unFollowTask(final long taskId, final GrouponCallback<Task> callback){
-		GrouponTask<Task> taskTask = new GrouponTask<Task>(callback) {
+	public void getFollowedTasks(GrouponCallback<ArrayList<Task>> callback) {
+		GrouponTask<ArrayList<Task>> taskTask = new GrouponTask<ArrayList<Task>>(callback) {
+			public ArrayList<Task> run() throws GrouponException {
+				String url = Constants.SERVER + "task/getFollowedTasks";
 
-			@Override
-			public Task run() throws GrouponException {
-				String url = Constants.SERVER+"task/unfollowTask";
-				Map<String,String> idMap= new HashMap<String, String>();
-				idMap.put("taskId", ""+taskId);
-				JSONObject json = ConnectionUtils.makePostRequest(url, idMap, app.getAuthToken());
-				Task task = new Task();
-				try {
-					task.setFollowerCount(json.getLong("followerCount"));
-				} catch (JSONException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+				JSONObject json = ConnectionUtils.makeGetRequest(url, null, app.getAuthToken());
+				ArrayList<Task> tasks = new ArrayList<Task>();
+				if (json.has("tasks")) {
+					try {
+						JSONArray tasksJson = json.getJSONArray("tasks");
+
+						for (int i = 0; i < tasksJson.length(); i++) {
+							tasks.add(convertJSONObjectToTask(tasksJson.getJSONObject(i)));
+						}
+					} catch (JSONException e) {
+						throw new GrouponException("An error occured while parsing json returned from the server!");
+					}
+				} else {
+					throw new GrouponException("An unknown error occured while creating the community :(");
 				}
-				return task;
+
+				return tasks;
 			}
-			
 		};
+
 		GrouponTask.execute(taskTask);
-	}	
-	public void followTask(final long taskId, final GrouponCallback<Task> callback){
+		
+	}
+	public void unFollowTask(final long taskId, final GrouponCallback<Task> callback) {
 		GrouponTask<Task> taskTask = new GrouponTask<Task>(callback) {
 
 			@Override
 			public Task run() throws GrouponException {
-				String url = Constants.SERVER+"task/followTask";
-				Map<String,String> idMap= new HashMap<String, String>();
-				idMap.put("taskId", ""+taskId);
+				String url = Constants.SERVER + "task/unfollowTask";
+				Map<String, String> idMap = new HashMap<String, String>();
+				idMap.put("taskId", "" + taskId);
 				JSONObject json = ConnectionUtils.makePostRequest(url, idMap, app.getAuthToken());
 				Task task = new Task();
 				try {
@@ -66,34 +74,58 @@ public class TaskService {
 				}
 				return task;
 			}
-			
+
 		};
 		GrouponTask.execute(taskTask);
 	}
+
+	public void followTask(final long taskId, final GrouponCallback<Task> callback) {
+		GrouponTask<Task> taskTask = new GrouponTask<Task>(callback) {
+
+			@Override
+			public Task run() throws GrouponException {
+				String url = Constants.SERVER + "task/followTask";
+				Map<String, String> idMap = new HashMap<String, String>();
+				idMap.put("taskId", "" + taskId);
+				JSONObject json = ConnectionUtils.makePostRequest(url, idMap, app.getAuthToken());
+				Task task = new Task();
+				try {
+					task.setFollowerCount(json.getLong("followerCount"));
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				return task;
+			}
+
+		};
+		GrouponTask.execute(taskTask);
+	}
+
 	public void createTask(final Task task, final long communityId, final GrouponCallback<Task> callback) {
 		GrouponTask<Task> taskTask = new GrouponTask<Task>(callback) {
 			public Task run() throws GrouponException {
 				String url = Constants.SERVER + "task/create";
 
-				JSONObject json = new JSONObject(); 
+				JSONObject json = new JSONObject();
 				try {
-					json.put("name",task.getName());
+					json.put("name", task.getName());
 					json.put("description", task.getDescription());
-					json.put("deadline",task.getDeadline());
-					
-					json.put("communityId",communityId);
-					
-					json.put("attributes",taskAttributestoJSONArray(task.getAttributes()));
-					json.put("type",task.getNeedType());
-					json.put("requirementName",task.getRequirementName());
+					json.put("deadline", task.getDeadline());
+
+					json.put("communityId", communityId);
+
+					json.put("attributes", taskAttributestoJSONArray(task.getAttributes()));
+					json.put("type", task.getNeedType());
+					json.put("requirementName", task.getRequirementName());
 					json.put("requirementQuantity", task.getRequirementQuantity());
-					json.put("tags",new JSONArray());
+					json.put("tags", new JSONArray());
 				} catch (JSONException e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
 
-				JSONObject jsonRes = ConnectionUtils.makePostRequest(url, null,json, app.getAuthToken());
+				JSONObject jsonRes = ConnectionUtils.makePostRequest(url, null, json, app.getAuthToken());
 
 				if (jsonRes.has("taskId")) {
 					try {
@@ -112,19 +144,27 @@ public class TaskService {
 
 		GrouponTask.execute(taskTask);
 	}
+
 	public void getTask(final long id, final GrouponCallback<Task> callback) {
 		GrouponTask<Task> taskTask = new GrouponTask<Task>(callback) {
 			public Task run() throws GrouponException {
-				String url = Constants.SERVER + "task/mobileShow/"+id;
+				String url = Constants.SERVER + "task/mobileShow/" + id;
 
 				JSONObject json = ConnectionUtils.makePostRequest(url, null, app.getAuthToken());
-				
+
 				Task task = null;
 				try {
-					
+
 					task = convertJSONObjectToTask(json.getJSONObject("task"));
-					if(json.has("isFollower"))
-						task.setFollower(json.getBoolean("isFollower"));				
+					
+					if (json.has("taskAttributes")) {
+						JSONObject j  = json.getJSONObject("taskAttributes");
+						Map<String, List<String>> m =convertJSONObjectToTaskAttribute(j);
+						task.setAttributeMap(m);
+					}
+					if (json.has("isFollower"))
+						task.setFollower(json.getBoolean("isFollower"));
+					
 				} catch (JSONException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
@@ -135,7 +175,32 @@ public class TaskService {
 
 		GrouponTask.execute(taskTask);
 
-	}	
+	}
+	private Map<String,List<String>> convertJSONObjectToTaskAttribute(JSONObject json) throws JSONException {
+	
+		Map<String, List<String>> m = new HashMap<String, List<String>>();
+		Iterator iter=json.keys();
+		while(iter.hasNext()){
+			String key = (String) iter.next();
+			Object o = json.get(key);
+			List<String> l= new ArrayList<String>();
+			
+			if(o instanceof String){
+				l.add((String)o);
+				m.put(key, l);
+			}
+			else if(o instanceof JSONArray){
+				JSONArray values = (JSONArray) o;
+				for(int i=0; i<values.length(); i++){
+					String value = values.getString(i);
+				
+					l.add(value);
+				}
+				m.put(key,l);
+			}
+		}
+		return m;
+	}
 	protected Task convertJSONObjectToTask(JSONObject json) throws JSONException {
 
 		Task task = new Task();
@@ -158,37 +223,39 @@ public class TaskService {
 		if (json.has("id")) {
 			task.setId(json.getLong("id"));
 		}
-		
-		if (json.has("ownerUsername")){
+
+		if (json.has("ownerUsername")) {
 			task.setOwnerUsername(json.getString("ownerUsername"));
 		}
-		if(json.has("ownerId"))
+		if (json.has("ownerId"))
 			task.setOwnerId(json.getLong("ownerId"));
-		if(json.has("communityName"))
+		if (json.has("communityName"))
 			task.setCommunityName(json.getString("communityName"));
-		if(json.has("needType"))
+		if (json.has("needType"))
 			task.setNeedType(json.getString("needType"));
-		if(json.has("status"))
+		if (json.has("status"))
 			task.setStatus(json.getString("status"));
-		if(json.has("deadline"))
-			task.setDeadlineCount(json.getLong("deadline"));
-		if(json.has("requirementName"))
+		if (json.has("deadline")){
+			Long l =json.getLong("deadline");
+			Date d = new Date(l);	
+			DateUtils.calculateDayDifference(d);
+			task.setDeadlineCount((long) DateUtils.calculateDayDifference(d));
+			
+		}
+		if (json.has("requirementName"))
 			task.setRequirementName(json.getString("requirementName"));
-		if(json.has("requirementQuantity"))
+		if (json.has("requirementQuantity"))
 			task.setRequirementQuantity(json.getInt("requirementQuantity"));
 
-		if(json.has("followerCount"))
+		if (json.has("followerCount"))
 			task.setFollowerCount(json.getLong("followerCount"));
 
-		
-		
 		return task;
 	}
 
-	private JSONArray taskAttributestoJSONArray(List<TaskAttribute> taskAttributes){
-		JSONArray array=new JSONArray();
-		
-		
+	private JSONArray taskAttributestoJSONArray(List<TaskAttribute> taskAttributes) {
+		JSONArray array = new JSONArray();
+
 		for (TaskAttribute taskAttribute : taskAttributes) {
 			JSONObject json = new JSONObject();
 			try {
@@ -202,4 +269,6 @@ public class TaskService {
 		}
 		return array;
 	}
+
+
 }
