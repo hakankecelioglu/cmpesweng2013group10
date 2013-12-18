ab23aab9bbd7330f3bfa996a88dee75447fba78e
serdarkuzucu13@gmail.com <serdarkuzucu13@gmail.com@b9fef26f-2b5a-7500-4f01-d936de0847ac>
2013-12-18 19:40:31 +0000

    dateutils added. day differences now can be calculated.

---
+++ trunk/GrouponMobile/src/com/groupon/mobile/utils/DateUtils.java
@@ -0,0 +1,15 @@
+package com.groupon.mobile.utils;
+
+import java.util.Date;
+
+import org.joda.time.Days;
+import org.joda.time.LocalDate;
+
+public class DateUtils {
+	public static int calculateDayDifference(Date date) {
+		LocalDate local1 = new LocalDate(date);
+		LocalDate local2 = new LocalDate();
+
+		return Days.daysBetween(local2, local1).getDays();
+	}
+}
