477006dbcce66ff2ce9a270ffffb6290ff69137d
serkanozer.591@gmail.com <serkanozer.591@gmail.com@b9fef26f-2b5a-7500-4f01-d936de0847ac>
2013-12-19 03:19:55 +0000

    Replyfield model

---
+++ trunk/GrouponMobile/src/com/groupon/mobile/model/ReplyFieldAttribute.java
@@ -0,0 +1,35 @@
+package com.groupon.mobile.model;
+
+
+public class ReplyFieldAttribute {
+	private String name;
+	private String value;
+	private ReplyField replyField;
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	public ReplyField getReplyField() {
+		return replyField;
+	}
+
+	public void setReplyField(ReplyField replyField) {
+		this.replyField = replyField;
+	}
+
+
+
+
+}
