1490245b64accd2c126e7d7d871e3cb100b4d12e
serdarkuzucu13@gmail.com <serdarkuzucu13@gmail.com@b9fef26f-2b5a-7500-4f01-d936de0847ac>
2013-12-29 11:50:47 +0000

    profile fragment was designed and implemented. waiting for web project to be deployed.

--- GrouponMobile/src/com/groupon/mobile/frag/ProfileFragment.java
+++ GrouponMobile/src/com/groupon/mobile/frag/ProfileFragment.java
@@ -9,13 +9,21 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.groupon.mobile.GrouponApplication;
 import com.groupon.mobile.R;
+import com.groupon.mobile.conn.GrouponCallback;
+import com.groupon.mobile.model.User;
+import com.groupon.mobile.service.UserService;
+import com.groupon.mobile.utils.StringUtils;
 
 public class ProfileFragment extends Fragment {
 	private boolean isInEditMode = false;
+	private Long profileOwnerId = -1L;
 
 	private Button editProfileButton;
+	private TextView usernameTextView;
 
 	private TextView nameTextView;
 	private TextView surnameTextView;
@@ -25,14 +33,27 @@ public class ProfileFragment extends Fragment {
 	private EditText nameEditText;
 	private EditText surnameEditText;
 
+	private GrouponApplication app;
+	private User user;
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		final View rootView = inflater.inflate(R.layout.profile, container, false);
+		app = (GrouponApplication) getActivity().getApplication();
+		user = app.getLoggedUser();
+
+		Bundle bundle = getArguments();
+		if (bundle != null) {
+			profileOwnerId = bundle.getLong("userId", -1);
+		}
+
 		setupUI(rootView);
+		setData();
 		return rootView;
 	}
 
 	public void setupUI(View rootView) {
+		usernameTextView = (TextView) rootView.findViewById(R.id.profile_username);
 		editProfileButton = (Button) rootView.findViewById(R.id.profile_edit_button);
 		nameTextView = (TextView) rootView.findViewById(R.id.profile_edit_text_name);
 		surnameTextView = (TextView) rootView.findViewById(R.id.profile_edit_text_surname);
@@ -43,6 +64,10 @@ public class ProfileFragment extends Fragment {
 		surnameEditText = (EditText) rootView.findViewById(R.id.profile_edit_text_surname_edit);
 
 		editProfileButton.setOnClickListener(editButtonClickListener);
+
+		if (!profileOwnerId.equals(-1L) && !user.getId().equals(profileOwnerId)) {
+			editProfileButton.setVisibility(View.GONE);
+		}
 	}
 
 	private OnClickListener editButtonClickListener = new OnClickListener() {
@@ -65,7 +90,7 @@ public class ProfileFragment extends Fragment {
 		surnameTextView.setVisibility(View.GONE);
 		surnameEditText.setVisibility(View.VISIBLE);
 
-		editProfileButton.setText("Save");
+		editProfileButton.setText(getString(R.string.profile_label_save_profile));
 	}
 
 	private void convertEditTextsToTextViews() {
@@ -75,12 +100,66 @@ public class ProfileFragment extends Fragment {
 		surnameTextView.setVisibility(View.VISIBLE);
 		surnameEditText.setVisibility(View.GONE);
 
-		editProfileButton.setText("Edit Profile");
+		editProfileButton.setText(getString(R.string.profile_label_edit_profile));
 	}
 
 	private void saveProfileInfoChanges() {
-		nameTextView.setText(nameEditText.getText().toString());
-		surnameTextView.setText(surnameEditText.getText().toString());
+		String name = nameEditText.getText().toString();
+		String surname = surnameEditText.getText().toString();
+
+		if (StringUtils.isNotBlank(name)) {
+			nameTextView.setText(name);
+		} else {
+			nameTextView.setText(" - ");
+		}
+
+		if (StringUtils.isNotBlank(surname)) {
+			surnameTextView.setText(surname);
+		} else {
+			surnameTextView.setText(" - ");
+		}
+	}
+
+	private void setData() {
+		if (profileOwnerId.equals(-1L) || user.getId().equals(profileOwnerId)) {
+			// My profile
+			putUserToView(user);
+		} else {
+			// Someone's profile
+			UserService service = new UserService(app);
+			service.getUserInfo(profileOwnerId, new GrouponCallback<User>() {
+				public void onSuccess(User response) {
+					putUserToView(response);
+				}
+
+				public void onFail(String errorMessage) {
+					Toast.makeText(app.getApplicationContext(), errorMessage, Toast.LENGTH_LONG).show();
+				}
+			});
+		}
+	}
+
+	private void putUserToView(User user) {
+		usernameTextView.setText(user.getUsername());
+		emailTextView.setText(user.getEmail());
+
+		if (user.getName() != null) {
+			nameTextView.setText(user.getName());
+			nameEditText.setText(user.getName());
+		} else {
+			nameTextView.setText(" - ");
+			nameEditText.setText("");
+		}
+
+		if (user.getSurname() != null) {
+			surnameTextView.setText(user.getSurname());
+			surnameEditText.setText(user.getSurname());
+		} else {
+			surnameTextView.setText(" - ");
+			surnameEditText.setText("");
+		}
+
+		reputationTextView.setText("0");
 	}
 
 }
